58b03414adc73e460b4374677abd6df9931bd5ed
Natt1122 <natalia.arboleda25@gmail.com>
2023-04-01 21:31:41 -0500

    App Ionic con Angular y Firebase

---
+++ README.md
@@ -0,0 +1,2 @@
+# Entregable-2
+Creacion de App en Ionic con Angular como Framework y Base de datos en Firebase
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+//Muy importante entender que en este archivo se relacionan los parámetros generados por Firebase
+export const environment = {
+  production: false,
+  FIREBASE_CONFIG:{
+    apiKey: "AIzaSyDcqHP82F85ATzRea-9BWzKnq1cfr4IB1o",
+    authDomain: "entregable2-79b28.firebaseapp.com",
+    projectId: "entregable2-79b28",
+    storageBucket: "entregable2-79b28.appspot.com",
+    messagingSenderId: "446729551407",
+    appId: "1:446729551407:web:e2308cf501f951ce8fd0da",
+    measurementId: "G-QWJG2RVSVG"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
